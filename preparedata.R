8b2c93c7f8f3569eabc680c9ed649032476ebff0
Frederik Hjorth <frederikhjorth@gmail.com>
2015-02-06 13:14:10 +0100

    names cleanup

--- preparedata.R
+++ preparedata.R
@@ -1,7 +1,8 @@
-setwd("/Users/frederikhjorth/GitHub/fv11-valgsteder")
+#setwd("/Users/frederikhjorth/GitHub/fv11-valgsteder") #mac
+setwd("/Users/fh/Documents/GitHub/fv11-valgsteder") #pc
 
 #gather all files in 1 df
-csvs<-list.files(pattern=".csv")
+csvs<-list.files(pattern=".csv")[2:11]
 allres<-data.frame(unit=NA,allvotes=NA,sd=NA,rv=NA,kf=NA,sf=NA,la=NA,kd=NA,df=NA,ve=NA,el=NA)
 for (f in csvs){
   dat<-read.csv(f,sep=";",skip=5,header=F,fileEncoding="latin1")[,2:12]
@@ -11,6 +12,20 @@ for (f in csvs){
 }
 allres<-allres[2:nrow(allres),]
 rm(f,dat,csvs)
+
+#remove higher level units
+allres<-allres[-which(grepl("STORKREDS",as.character(allres$unit))),]
+allres<-allres[-which(grepl("OPSTILLINGSKREDS",as.character(allres$unit))),]
+
+#standardize names
+allres$unit<-tolower(allres$unit)
+
+allres$unit<-gsub("ã¸","oe",allres$unit)
+allres$unit<-gsub("ã¦","ae",allres$unit)
+allres$unit<-gsub("ã\u0098","oe",allres$unit)
+allres$unit<-gsub("ã¥","aa",allres$unit)
+allres$unit<-gsub("ã","aa",allres$unit)
+allres$unit<-gsub("ã¼","u",allres$unit)
 
 #ex.: reddest voting places in DK?
 allres$redshare<-(allres$sd+allres$sf+allres$el)/allres$allvotes
